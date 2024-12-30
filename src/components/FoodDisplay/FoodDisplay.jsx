5a713e12c666396fbed384a96a6f15bb91f57f03
TRKUyen <kimuyen425@gmail.com>
2024-12-30 08:17:28 +0700

    Add Food Delivery Project to repo web project

---
+++ src/components/FoodDisplay/FoodDisplay.jsx
@@ -0,0 +1,30 @@
+import { useContext } from 'react'
+import PropTypes from 'prop-types'
+import './FoodDisplay.css'
+import { StoreContext } from '../../context/StoreContext'
+import FoodItem from '../FoodItem/FoodItem'
+
+
+const FoodDisplay = ({category}) => {
+
+    const { food_list } = useContext(StoreContext);
+
+  return (
+    <div className='food-display' id='food-display'> 
+      <h2>Top dishes near you</h2>
+      <div className="food-display-list">
+        {food_list.map((item, index) => {
+            if (category === item.category || category === 'All' ) {
+                return <FoodItem key={index} id={item.id} name={item.name} price={item.price} description={item.description} image={item.image} />
+            } 
+        })}
+      </div>
+    </div>
+  )
+}
+
+FoodDisplay.propTypes = {
+  category: PropTypes.string.isRequired,
+}
+
+export default FoodDisplay
